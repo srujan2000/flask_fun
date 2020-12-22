ce3f48306cb112f878f10dcfce3c45b8b7897f83
srujan <srujanreddy168@gmail.com>
2020-12-22 18:31:58 +0530

    changed mail and pass

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -8,7 +8,7 @@ from flask_mail import Mail
 app = Flask(__name__)
 
 
-app.config['SECRET_KEY'] = 'aewuuhwu112ii313'
+app.config['SECRET_KEY'] = os.environ.get('flask_key')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
@@ -18,8 +18,8 @@ login_manager = LoginManager(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'srujanreddy168@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ugxqczucgycfzzur'
+app.config['MAIL_USERNAME'] = os.environ.get('flask_email')
+app.config['MAIL_PASSWORD'] = os.environ.get('flask_password')
 mail = Mail(app)
 
 from flaskblog import routes
